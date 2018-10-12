e0fe0b5682bd06f5c0a4aadf69f7682a7524ff15
PampuchSkrytozerca <stanosz.mariusz@gmail.com>
2018-10-12 18:44:42 +0200

    Updates

---
+++ precache-manifest.aa59ff2705fd861440704d76e374b8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa3a1b47ecfa96bfa9ad",
+    "url": "/To-Do-App/static/js/runtime~main.fa3a1b47.js"
+  },
+  {
+    "revision": "4aab271995774f38bde3",
+    "url": "/To-Do-App/static/js/main.4aab2719.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/To-Do-App/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "4aab271995774f38bde3",
+    "url": "/To-Do-App/static/css/main.9d86f619.chunk.css"
+  },
+  {
+    "revision": "18baf6424e35c13ed71740f15637a0f0",
+    "url": "/To-Do-App/index.html"
+  }
+];
